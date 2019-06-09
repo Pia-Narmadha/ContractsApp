fa5d6eba57cf6e15a4c2e89d6b00c7290539e8b6
Pia-Narmadha <meerajen.narmadha@gmail.com>
2019-06-09 15:18:33 -0400

    Initial commit of contracts app with mock json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contractsApp';
+
+  contracts = {};
+
+  constructor(private http: HttpClient){}
+  ngOnInit() {
+
+    this.http.get("https://localhost:4200/api/contracts")
+      .subscribe((data: String) => {
+        console.log("get worked:",data);
+        this.contracts = data;
+      });
+      
+      console.log("contracts:",this.contracts);
+    }
+
 }
